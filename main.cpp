2327c99e66ffcb49711d80dac357dd5041a229ed
CosminaCasiu <casiu.stefania.x9d@student.ucv.ro>
2024-11-07 19:52:01 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -3,21 +3,17 @@ using namespace std;
 int main()
  {
     Mentorat mentorat; 
-
-    // Adăugăm studenți
+  
     mentorat.addStudent(Student("Cosminaaa Casiu", 1));
     mentorat.addStudent(Student("Serban Denisa", 2));
     mentorat.addStudent(Student("Maria Alexandra", 3));
     mentorat.addStudent(Student("Daniela Andreea", 4)); 
 
-    // Adăugăm mentori
     mentorat.addMentor(Mentor("Mentor POO", 101, 2));
     mentorat.addMentor(Mentor("Mentor ASD", 102, 2)); 
 
-    // Alocăm studenții 
     mentorat.allocateStudents(); 
 
-    // Afișăm alocările
     mentorat.displayAllocations(); 
 
     return 0;
